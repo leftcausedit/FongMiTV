a9cdb73d568534679b92b92ee3de8668d9b5da4c
lefty <leftcausedit@gmail.com>
2024-01-12 16:53:37 +0800

    improve seek

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -1670,7 +1670,8 @@ public class VideoActivity extends BaseActivity implements Clock.Callback, Custo
     public void onSeek(int time) {
         mBinding.widget.action.setImageResource(time > 0 ? R.drawable.ic_widget_forward : R.drawable.ic_widget_rewind);
         mBinding.widget.seek.setVisibility(View.VISIBLE);
-        mBinding.widget.time.setText(mPlayers.getPositionTime(time));
+        String finaltime = mPlayers.getPositionTime(time) + " [" + (time > 0 ? "+" : "") + mPlayers.stringToTime(time) + "]";
+        mBinding.widget.time.setText(finaltime);
         hideProgress();
     }
 
